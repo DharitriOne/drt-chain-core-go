2752726e66e03d0b99cbf0ae7974f3cacec37d94
xfactor-toml <xfactor.toml@gmail.com>
2024-08-22 15:52:16 -0400

    feat: regenerate go code with casttypewith

--- data/dct/dct.go
+++ data/dct/dct.go
@@ -4,8 +4,8 @@ package dct
 import "math/big"
 
 // New returns a new batch from given buffers
-func New() *ESDigitalToken {
-	return &ESDigitalToken{
+func New() *DCToken {
+	return &DCToken{
 		Value: big.NewInt(0),
 	}
 }
